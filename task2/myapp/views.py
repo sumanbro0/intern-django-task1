dc953d1c271734088ffce01173659aa1f2d63f09
sumanbro0 <pandey.suman258@gmail.com>
2023-12-13 20:39:06 +0545

    completed task 2

---
+++ task2/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Author, Book
+# Create your views here.
+def index(request):
+    books=Book.objects.all()
+    authors=Author.objects.all()
+    aut=authors[0].books.all()
+    print(aut)
+    return render(request,"index.html",{"books":books,"authors":authors})
